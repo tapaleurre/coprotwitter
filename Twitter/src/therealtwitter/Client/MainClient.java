0cd40343baa82fe479623271af01c999f0575d8c
Simon Foëx <tapaleurre@hotmail.fr>
2018-06-04 16:20:10 +0200

    Corba server implemented

--- Twitter/src/therealtwitter/Client/MainClient.java
+++ Twitter/src/therealtwitter/Client/MainClient.java
@@ -13,22 +13,22 @@ public class MainClient {
         // Paramétrage pour la création de la couche ORB :
         // localisation de l'annuaire d'objet (service nommage)
         Properties props = new Properties();
-        props.put("org.omg.CORBA.ORBInitialPort", "1337");
-        props.put("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
+        props.put("org.omg.TwitterService.ORBInitialPort", "1337");
+        props.put("org.omg.TwitterService.ORBInitialHost", "127.0.0.1");
         // Création de la couche ORB
-        // pour communiquer via un bus CORBA
+        // pour communiquer via un bus TwitterService
         ORB orb = ORB.init((String[]) null, props);
         // Recherche d'une référence sur un service de nommage
         org.omg.CORBA.Object serviceNommageRef;
         serviceNommageRef = orb.resolve_initial_references("NameService");
         // Instance du service de nommage à partir de sa référence
-        // ("cast" façon CORBA)
+        // ("cast" façon TwitterService)
         NamingContextExt serviceNommage = NamingContextExtHelper.narrow(serviceNommageRef);
         /** Etape 3 **/
         // Recherche d'une référence sur le service météo
         org.omg.CORBA.Object monServiceRef;
         //monServiceRef = namingContext.resolve_str("MonService");
-        //TODO: Instance du service Météo à partir de sa référence (cast)
+        //TODO: Instance du service Twitter à partir de sa référence (cast)
         //MonService monService = MonServiceHelper.narrow(monServiceRef);
 
     }
