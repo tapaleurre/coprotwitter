b49a9b063b2cde18ea1cfde3b0726d01889967bb
heror <heror38@gmail.com>
2018-06-04 17:51:02 +0200

    RMI Utiliseur + un peu de classe

---
+++ Twitter/src/therealtwitter/Serveur/TwitterDBServiceImpl.java
@@ -0,0 +1,76 @@
+package therealtwitter.Serveur;
+
+import javax.rmi.CORBA.Util;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwitterDBServiceImpl extends UnicastRemoteObject implements TwitterDBService {
+
+    private List<Utilisateur> utilisateurs;
+    private List<String> nomUtilisateurs;
+    private Utilisateur utilisateur;
+
+
+    public TwitterDBServiceImpl() throws RemoteException {
+
+        utilisateurs = new ArrayList<Utilisateur>();
+
+    }
+
+
+
+    @Override
+    public Utilisateur connect(String username, String password) throws RemoteException {
+        return (loggedUser(username, password));
+    }
+
+    @Override
+    public boolean existsTarget(String targetName) throws RemoteException {
+        return existsUser(targetName);
+    }
+
+    private Utilisateur findUser(String username){
+        for (Utilisateur u : utilisateurs)
+            if (u.getUsername().equals(username))
+                return u;
+        return null;
+    }
+
+    @Override
+    public boolean existsUser(String username){
+        return findUser(username)!=null;
+    }
+
+
+    private Utilisateur loggedUser(String username, String password) {
+        for (Utilisateur u : utilisateurs)
+            if (u.getUsername().equals(username) && u.getPassword().equals(password))
+                return u;
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return "Je suis un twitter de "+utilisateurs.size()+" users ";
+    }
+
+
+
+    @Override
+    public List<String> getAllUsers() throws RemoteException{
+        return nomUtilisateurs;
+    }
+    @Override
+    public void createNewUser(String username, String password) throws RemoteException{
+        if (existsUser(username)) {
+            Utilisateur uti = new Utilisateur(username, password);
+        }
+    }
+    public boolean isUserPasswordCorrect(String username, String password) throws RemoteException{
+        Utilisateur.getUsername()
+    }
+
+
+}
---
+++ Twitter/src/therealtwitter/Serveur/Utilisateur.java
@@ -0,0 +1,72 @@
+package therealtwitter.Serveur;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utilisateur implements Serializable {
+
+
+    private String username;
+    private String password;
+    private List<Utilisateur> followedUsers;
+
+
+
+    public Utilisateur(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.followedUsers=new ArrayList<>();
+        this.followedUsers.add(this);
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Permet de returner le nom de l'utilisateur
+     * @return string : name
+     */
+    @Override
+    public String toString() {
+        String twittos = "Twittos : "+username+" \n";
+        for (Utilisateur u : followedUsers)
+            twittos+="\t@"+u.getUsername()+"\n";
+        return twittos;
+    }
+
+    private boolean isFollowingUser(String user){
+        System.out.println("\tisFollowingUser call answer : "+getUtilisateur(user)!=null);
+        return getUtilisateur(user)!=null;
+    }
+
+    public void followUser(Utilisateur user){
+        System.out.println("follow user :");
+        if (!isFollowingUser(user.getUsername())){
+            this.followedUsers.add(user);
+            System.out.println("user added");
+        }else System.out.println("user already added");
+    }
+
+    private Utilisateur getUtilisateur(String user){
+        System.out.println("SEARCHING : "+user);
+        for (Utilisateur u : followedUsers)
+            if (u.getUsername().equals(user)){
+                System.out.println("\tgetUser call answer : "+u);
+                return u;
+            }
+        System.out.println("\ton a pas trouvé "+user );
+        return null;
+
+    }
+
+    public List<Utilisateur> getFollowedUsers() {
+        return followedUsers;
+    }
+}
