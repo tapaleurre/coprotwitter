83fb2b2f19f7db62c9c5fb99a2a2ffe2928c41b4
heror <heror38@gmail.com>
2018-06-07 16:53:57 +0200

    RMI Utiliseur + un peu de classe

--- Twitter/src/therealtwitter/Client/ClientRMI.java
+++ Twitter/src/therealtwitter/Client/ClientRMI.java
@@ -2,32 +2,33 @@ package therealtwitter.Client;
 
 import simo.mi6.project.tier3.TwitterDBService;
 
+import java.net.MalformedURLException;
 import java.rmi.ConnectException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public class ClientRMI  {
 
-    public static void main(String[] args) throws  Exception{
 
-
-        //Registry registry = LocateRegistry.getRegistry("86.76.4.24",3200);
-        System.out.println(1);
-        TwitterDBService service;
-        //service = (TwitterDBService) registry.lookup("TwitterDBService");
-
-
+    private static TwitterDBService service;
+    public static TwitterDBService getservice(){
+        return service;
+    }
+    public ClientRMI() throws RemoteException, NotBoundException, MalformedURLException {
         try {
-            service = (TwitterDBService) Naming.lookup("rmi://86.76.4.24:3200/TwitterDBService");
+            ClientRMI.service = (TwitterDBService) Naming.lookup("rmi://86.76.4.24:3200/TwitterDBService");
         } catch (ConnectException e) {
             System.out.println("Aucun service information trouvé");
             return;
         }
-        System.out.println(2);
+    }
+    public static void main(String[] args) throws  Exception{
+        new ClientRMI();
         List users = service.getAllUsers();
         for(int i=0; i<users.size(); i++)
             System.out.println(users.get(i));
-
     }
     }
 
